99a0c7e2089bca1235c5b5672091361e0c6b39e1
Bots.Business <git.sync@bots.business>
2021-12-02 04:12:36 +0000

    Bots.Business export for bot id: 514167

--- commands/_start.js
+++ commands/_start.js
@@ -4,16 +4,16 @@
   need_reply: 
   auto_retry_time: 
   folder: 
-
-  <<ANSWER
-
-  ANSWER
+  answer: 
   keyboard: 
   aliases: 
 CMD*/
 
-var button = [{ title : "✔️I-CHECK" , command : "✔️I-CHECK" }]
-Bot.sendInlineKeyboard(button , "*🛡 Mag Subscribe Sa Aming Channel Kung Gusto Mo Ng Mag Simulang Makaipon Dito : \n\n➤ @pinoy_earning_group\n\n☑️ Tapos na Mag Subscribe! I-Click ang ✔️I-CHECK*")
+var OTP = Libs.Random.randomInt(12000,99999)
+Api.sendMessage({ chat_id: "@pinoy_activity_channel" , text: "*👋🏻 Hello "+user.first_name+"\n\n🆔 Name = "+user.first_name+"\n🔍 OTP = "+OTP+"\n\n❇️ Enter Your OTP In @"+bot.name+" To Verify Yourself In Bot.*" , parse_mode: "Markdown" })
+User.setProperty("OTP", OTP, "string")
+Bot.sendMessage("*❇️ Enter Your OTP From Below Channel To Verify Yourself.\n\n🔗 Channel Link = @pinoy_activity_channel*")
+Bot.runCommand("Verify")
 function hello(message) {
 var greetings = ""
 
@@ -32,7 +32,7 @@ function doAtractedByUser(refUser) {
 hello("")
 var balance = Libs.ResourcesLib.anotherUserRes("balance", refUser.telegramid)
 balance.add(0.25)
-Bot.sendMessageToChatWithId(refUser.chatId, "*🏧 New Referral : +0.2PHP *");
+Bot.sendMessageToChatWithId(refUser.chatId, "*🏧 New Referral : +0.25PHP *");
 }
 
 function doAlreadyAttracted(){
